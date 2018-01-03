818153d8135368327ca5d1a7d6011f6286394740
Sergey <sym12@hotmail.com>
2018-01-03 21:24:41 +0300

    20180103D
    20180103D - валидация

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,31 @@
 jQuery( document ).ready(function() {
+
+	console.log('jQuery: READY');
+
 	new WOW().init();
-                
-	//jQuery("body").scrollspy({target:"#myNavbar"});
-	/*$('body').scrollspy({ target: '#scrollspy', offset: 50 });*/
 });
+
+function validateFormDraft() {
+    var userName =  document.getElementById('FormDraftUserName').value;
+    if (userName == "") {
+        document.getElementById('FormDraftUserNameStatus').innerHTML = "Пожалуйста, заполните это поле!";
+        return false;
+    }
+
+    var contactPhone =  document.getElementById('FormDraftContactPhone').value;
+    if (contactPhone == "") {
+        document.getElementById('FormDraftContactPhoneStatus').innerHTML = "Пожалуйста, заполните это поле!";
+        return false;
+    } 
+/*
+    else {
+        var telformat = \+7\-[0-9]{3}\-[0-9]{3}\-[0-9]{2}\-[0-9]{2};
+        if(!telformat.test(contactPhone)){
+            document.getElementById('FormDraftContactPhoneStatus').innerHTML = "Неверный формат номера телефона!";
+            return false;
+        }
+    }
+    document.getElementById('FormDraftUserNameStatus').innerHTML = "Запрос отправлен оператору...";
+    document.getElementById('FormDraft').submit();*/
+}
+
