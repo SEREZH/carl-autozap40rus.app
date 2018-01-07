8a55cf83fbb065a93bf673c6e9f30e929674be5b
Sergey <sym12@hotmail.com>
2018-01-07 10:41:57 +0300

    20180107D
    20180107D - уже заполняются Модели из БД

--- public/php/ez-car-autocompl.php
+++ public/php/ez-car-autocompl.php
@@ -9,11 +9,24 @@ if (file_exists($filename)) {
 include $filename;
 $test = array($dbName);//echo json_encode($test, JSON_UNESCAPED_UNICODE); // как бы руссификация :)
 
-
+//находим id_car_mark по переданному наименованию марки car_mar.name
 if (isset($_POST["car_mark"]))    { $car_mark   = $_POST["car_mark"];}      else{ $car_mark  = '';}
-//формируем текст запроса
-$sqlCommand = "select id_car_model, name from car_model where id_car_mark=2 order by name";
-$sqlResult  = mysqli_query($connConnection, $sqlCommand) or die (mysqli_error($connConnection));
+// инициализируем индексный массив Модели
+$arModelRows = array();
+//array_push($arModelRows, $car_mark);
+if ($car_mark!='') {
+	//формируем текст запроса для выборки Моделей для Марки
+	$sqlCommandCarModel = "select id_car_model, name from car_model where id_car_mark = (select id_car_mark from car_mark where name='$car_mark' limit 1) order by name";
+	//array_push($arModelRows, $sqlCommandCarModel);
+	$sqlResultCarModels  = mysqli_query($connConnection, $sqlCommandCarModel) or die (mysqli_error($connConnection));		
+	//array_push($arModelRows, $sqlResultCarModels);
+	while ($row = mysqli_fetch_array($sqlResultCarModels, MYSQLI_NUM)) {
+		array_push($arModelRows, $row[1]);
+	}
+}
+echo json_encode($arModelRows, JSON_UNESCAPED_UNICODE); // как бы руссификация :)
+
+
 /*if ($sqlResult.count()) {
 	$arModelRows = array("Jaguar","Jeep");
 	array_push($arModelRows, "Isdera");
@@ -21,14 +34,6 @@ $sqlResult  = mysqli_query($connConnection, $sqlCommand) or die (mysqli_error($c
 	$arModelRows = array("Jaguar","Jeep","Jensen");
 	array_push($arModelRows, "Koenigsegg");
 }*/
-
-$arModelRows = array();
-while ($row = mysqli_fetch_array($sqlResult, MYSQLI_NUM)) {
-    array_push($arModelRows, $row[1]);
-}    
-
-
-echo json_encode($arModelRows, JSON_UNESCAPED_UNICODE); // как бы руссификация :)
 
 //не выдал ли нам запрос ошибки 
 /*if (!$sqlResultCount) {
